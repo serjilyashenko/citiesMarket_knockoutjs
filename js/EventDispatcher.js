4bdca0c428039a3f3ffe13dad4bdd93b71ee1251
sky <sky.ilyashenko@gmail.com>
2016-06-12 16:25:33 +0300

    add EventDispatcher

---
+++ js/EventDispatcher.js
@@ -0,0 +1,21 @@
+function EventDispatcher() {
+    this.subscriptors = {};
+}
+
+EventDispatcher.prototype.subscribe = function (event, callback) {
+    var subscriptors = this.subscriptors;
+    if (!subscriptors[event]) {
+        subscriptors[event] = [];
+    }
+    subscriptors[event].push(callback);
+};
+
+EventDispatcher.prototype.trigger = function (event) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    var subscriptors = this.subscriptors;
+    if (subscriptors[event]) {
+        subscriptors[event].forEach(function (callback) {
+            callback.apply(null, args);
+        });
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 (function () {
 
-    var citiesList = new CitiesList();
-    
-    // TODO: remove
+    var eventDispatcher = new EventDispatcher();
 
+    var citiesList = new CitiesList();
     ko.applyBindings(citiesList, document.getElementById('cities-list'));
+
+
+    // TODO: remove
+    window.eventDispatcher = eventDispatcher;
+
+    eventDispatcher.subscribe('change1', function (param) {
+        console.log('callback1 of change1 with param = ', param);
+    });
+    eventDispatcher.subscribe('change2', function (amount, currency) {
+        console.log('callback1 of change2 with param = ', amount, currency);
+    });
+
+    eventDispatcher.trigger('change1', 'paramA');
+    eventDispatcher.trigger('change2', 'paramB');
+
+    eventDispatcher.subscribe('change1', function (param) {
+        console.log('callback2 of change1 with param = ', param);
+    });
+
+    eventDispatcher.trigger('change1', "paramC");
+    eventDispatcher.trigger('change2', 100000, 'usd');
 }());
