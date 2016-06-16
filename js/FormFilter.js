652a442d2e767cc53816b1328bbc6764462ea4c0
sky <sky.ilyashenko@gmail.com>
2016-06-16 17:19:40 +0300

    FormFilter feedback from server

--- js/FormFilter.js
+++ js/FormFilter.js
@@ -1,12 +1,21 @@
 function FormFilter(eventDispatcher) {
+    var self = this;
     this.eventDispatcher = eventDispatcher;
-    
+
     this.populationMin = ko.observable(100);
     this.populationMax = ko.observable(25000000);
     this.yearMin = ko.observable(0);
     this.yearMax = ko.observable(2016);
+
+    eventDispatcher.subscribe('server: dataGot', function (data) {
+        self.populationMin(data.meta.populationMin);
+        self.populationMax(data.meta.populationMax);
+        self.yearMin(data.meta.yearMin);
+        self.yearMax(data.meta.yearMax);
+    });
 }
 
 FormFilter.prototype.submitForm = function () {
+    // TODO: why it see parameter eventDispatcher ???
     eventDispatcher.trigger('formFilter: submit', this);
 };
